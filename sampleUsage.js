4fd760454360e18fc8f47782d7f8bbe04cd9ae6f
kentan <hyperkentakun@gmail.com>
2014-11-25 22:01:47 -0700

    Added the tileDrawing.js with sample html and js.

---
+++ sampleUsage.js
@@ -0,0 +1,41 @@
+function startDrawing(){
+      tileDrawing.drawCharactor1("canvas1");
+      tileDrawing.drawCharactor2("canvas2"); 
+      tileDrawing.drawCharactor3("canvas3"); 
+      tileDrawing.drawCharactor4("canvas4"); 
+      tileDrawing.drawCharactor5("canvas5"); 
+      tileDrawing.drawCharactor6("canvas6"); 
+      tileDrawing.drawCharactor7("canvas7"); 
+      tileDrawing.drawCharactor8("canvas8"); 
+      tileDrawing.drawCharactor9("canvas9"); 
+
+      tileDrawing.drawBamboo1("canvas10");
+      tileDrawing.drawBamboo2("canvas11"); 
+      tileDrawing.drawBamboo3("canvas12"); 
+      tileDrawing.drawBamboo4("canvas13"); 
+      tileDrawing.drawBamboo5("canvas14"); 
+      tileDrawing.drawBamboo6("canvas15"); 
+      tileDrawing.drawBamboo7("canvas16"); 
+      tileDrawing.drawBamboo8("canvas17"); 
+      tileDrawing.drawBamboo9("canvas18"); 
+
+      tileDrawing.drawCircle1("canvas19");
+      tileDrawing.drawCircle2("canvas20");   
+      tileDrawing.drawCircle3("canvas21");   
+      tileDrawing.drawCircle4("canvas22");   
+      tileDrawing.drawCircle5("canvas23");   
+      tileDrawing.drawCircle6("canvas24");   
+      tileDrawing.drawCircle7("canvas25");   
+      tileDrawing.drawCircle8("canvas26");   
+      tileDrawing.drawCircle9("canvas27"); 
+
+      tileDrawing.drawEast("canvas28");
+      tileDrawing.drawSouth("canvas29");   
+      tileDrawing.drawWest("canvas30");   
+      tileDrawing.drawNorth("canvas31");   
+      tileDrawing.drawDragon1("canvas32");   
+      tileDrawing.drawDragon2("canvas33");   
+      tileDrawing.drawDragon3("canvas34");   
+
+}
+
